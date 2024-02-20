e296e9f670ac3e446ea07d3b9c60bf4dc8a28c0c
RyoyaKawano <54755448+RyoyaKawano@users.noreply.github.com>
2024-02-20 07:42:56 +0000

    f

--- app.py
+++ app.py
@@ -12,14 +12,13 @@ def get_info_details(ip_addr):
     api_geolocation = 'e09ab315a1e64d67a73ce8bf111b5e55'
     url = f"https://ipinfo.io/{ip_addr}?token={api_key}"
     response = requests.get(url)
-    data = response.json()
     
     url = f"https://api.ipgeolocation.io/ipgeo?apiKey={api_geolocation}&ip={ip_addr}"
     response2 = requests.get(url)
-    data2 = response2.json()
 
     if response.status_code == 200:
         data = response.json()
+        data2 = response2.json()
         info = {
             'ip': data.get('ip'),
             'hostname': data.get('hostname'),
--- models.py
+++ models.py
@@ -1,14 +1,15 @@
-import requests
-from flask import Flask, render_template, request
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float
-from flask_sqlalchemy import SQLAlchemy
+    ip_addr = "8.8.8.8"
+    api_geolocation = 'e09ab315a1e64d67a73ce8bf111b5e55'
+    url = f"https://ipinfo.io/{ip_addr}?token={api_key}"
+    response = requests.get(url)
+    data = response.json()
+    
+    url = f"https://api.ipgeolocation.io/ipgeo?apiKey={api_geolocation}&ip={ip_addr}"
+    response2 = requests.get(url)
+    data2 = response2.json()
 
-from app import db
-
-class WeatherData(db.Model):
-    id = Column(Integer, primary_key=True)
-    city = Column(String(80), nullable=False)
-    description = Column(String(120))
-    temperature = Column(Float)
+    if response.status_code == 200:
+        data = response.json()
+        info = {
+            'language':data2.get('language')
+        }
