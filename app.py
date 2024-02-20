1095fae37923d6f7fa8138f1627de98be085b5c1
VimeansethThorng <117240605+VimeansethThorng@users.noreply.github.com>
2024-02-20 05:39:05 +0000

    seth

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import requests
-import geocoder
 import json
 from flask import Flask, render_template, request, jsonify
 
@@ -62,14 +61,8 @@ def index():
 
         # Extract weather info if the API call is successful, else error
         if data['cod'] == '404':
-            return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None)
-            # return render_template('index.html', city_list=None, weather_list=None, temp_list=None, 
-            #                        ip=info['ip'], hostname=info['hostname'], 
-            #                        city=info['city'], region=info['region'], country=info['country'],
-            #                        log=info['loc'], org=info['org'], postal=info['postal'],
-            #                        timezone=info['timezone'], asn=info['asn'], company=['company'],
-            #                        privacy=info['privacy'], abs=info['abs'],
-            #                        )
+            return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None,
+                                   ip_address=ip_address)
         else:
             weather = data['weather'][0]['description']
             temp = data['main']['temp']
@@ -85,41 +78,10 @@ def index():
             with open('data/data.json', 'w') as f:
                 json.dump(data_json, f, indent=3)
 
-            return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None)
-    
-    elif request.method == 'POST':
-        # with open('data/data.json', encoding='utf-8') as f:
-        #     previous_data = json.load(f)
-        # if(city is None and previous_data is None):
-        #     return render_template('index.html')
-        # if(previous_data is not None):
-        #     city_list = previous_data["city"]
-        #     weather_list = previous_data["weather"]
-        #     temp_list = previous_data["temp"]
-
-        # url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
-        # data = requests.get(url).json()
-
-        # # Extract weather info if the API call is successful, else error
-        # if data['cod'] == '404':
-        #     return render_template('index.html', city_list=city_list, weather_list=weather_list, temp_list=temp_list)
-        # else:
-        #     weather = data['weather'][0]['description']
-        #     temp = data['main']['temp']
-        #     city_list.append(city)
-        #     weather_list.append(weather)
-        #     temp_list.append(temp)
-
-        #     data_json = {
-        #         'city': city_list,
-        #         'weather': weather_list,
-        #         'temp':temp_list
-        #     }
-        #     with open('data/data.json', 'w') as f:
-        #         json.dump(data_json, f, indent=3)
-
-        return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None)
-    else: return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None)
+            return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None,
+                                   ip_address=ip_address)    
+    else: return render_template('index.html', info=info, city_list=None, weather_list=None, temp_list=None,
+                                 ip_address=ip_address)
 
 
 if __name__ == '__main__':
